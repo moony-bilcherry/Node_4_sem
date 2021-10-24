f452ccf52e6b3d95603d99c250afc26627c4065d
moony-bilcherry <erchinskaya.nataliya@gmail.com>
2021-10-24 17:43:37 +0300

    04 done

--- node_lab04/04-01.js
+++ node_lab04/04-01.js
@@ -5,29 +5,42 @@ const url = require('url');
 let data = require('./db.js');
 let db = new data.DB();
 
+// слушатели событий
+
 db.on('GET', (request, response) => {
-	console.log('DB.GET called');
+	console.log('GET called');
 	response.end(JSON.stringify(db.select()));
 })
 
 db.on('POST', (request, response) => {
-	console.log('DB.POST called');
+	console.log('POST called');
 	request.on('data', data => {
         let row = JSON.parse(data);
         row.id = db.getIndex();
-        db.insert(row);
-        response.end(JSON.stringify(row));
+        response.end(JSON.stringify(db.insert(row)));
     });
 })
 
 db.on('PUT', (request, response) => {
-	console.log('DB.PUT called');
-	
+	console.log('PUT called');
+	request.on('data', data => {
+        let row = JSON.parse(data);
+        response.end(JSON.stringify(db.update(row)));
+    });
 })
 
 db.on('DELETE', (request, response) => {
-	console.log('DB.DELETE called');
-	
+	console.log('DELETE called');
+	let id = url.parse(request.url, true).query.id;
+	if(url.parse(request.url, true).query.id === undefined) {
+		response.end('{"ERROR": "parameter not provided"}');
+	}
+	if (url.parse(request.url, true).query.id !== null) {
+		let id = +url.parse(request.url, true).query.id;
+		if (Number.isInteger(id)) {
+			response.end(JSON.stringify(db.delete(id)));
+		}
+	}
 })
 
 http.createServer(function (request, response) {
@@ -35,6 +48,11 @@ http.createServer(function (request, response) {
 		// генерация события, имя события - строка request.method
         db.emit(request.method, request, response);
   	}
+	if (request.url === '/') {
+        let html = fs.readFileSync('./04-02.html');
+        response.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'});
+        response.end(html)
+    }
 }).listen(5000);
 
 console.log('Server running at http://localhost:5000/api/db');
--- node_lab04/db.js
+++ node_lab04/db.js
@@ -2,7 +2,7 @@ const util = require('util');   // inherits
 const ee = require('events');   // EventEmitter
 
 let db_data = [
-    { id: 1, name: 'SHinji Ikari', bday: '2001-06-06' },
+    { id: 1, name: 'Shinji Ikari', bday: '2001-06-06' },
     { id: 2, name: 'Rei Ayanami', bday: '2001-03-30' },
     { id: 3, name: 'Asuka Langley Soryu', bday: '2001-12-04' },
     { id: 4, name: 'Misato Katsuragi', bday: '1986-12-08' }
@@ -12,8 +12,26 @@ function DB() {
     this.getIndex = () => { return db_data.length + 1; };
 
     this.select = () => { return db_data; }
-    this.insert = (row) => { db_data.push(row); }
-
+    this.insert = (row) => { 
+        console.log(row);
+        db_data.push(row); 
+        return row; 
+    }
+    this.update = (row) => { 
+        console.log(row);
+        let index = db_data.findIndex(el => el.id === parseInt(row.id));
+        if(index === -1) return JSON.parse('{"ERROR": "id not found"}');
+        db_data.splice(index, 1, row);
+        return row;
+    }
+    this.delete = (id) => { 
+        console.log(id);
+        let index = db_data.findIndex(el => el.id === parseInt(id));
+        if(index === -1) return JSON.parse('{"ERROR": "id not found"}');
+        let del_row = db_data[index];
+        db_data.splice(index, 1);
+        return del_row;
+    }
 }
 
 util.inherits(DB, ee.EventEmitter);
